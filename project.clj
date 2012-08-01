1ab7e970e1e9e57ab6eec16349596d8ef72b74ab
Murphy McMahon <pandeiro@gmail.com>
2012-08-01 14:33:39 -0300

    updated :url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject casper-cljs/lein-template "0.1.0"
   :description "Basic CasperJS project template"
-  :url "https://example.com"
+  :url "https://github.com/pandeiro/casper-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
