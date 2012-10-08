443efe4eef28255a1a85f631628cddc288d2ad47
Murph <pandeiro@gmail.com>
2012-10-08 00:07:35 -0300

    beefed up core casper script skeleton and added link to API for reference

--- src/leiningen/new/casper_cljs/core.cljs
+++ src/leiningen/new/casper_cljs/core.cljs
@@ -1,4 +1,15 @@
 (ns {{name}}.core
   (:require [ghost.core :as casper]))
 
+;;
+;; For complete wrapper API, see:
+;; https://github.com/pandeiro/ghost/blob/master/src/ghost/core.cljs#L28
+;;
+(casper/start
+ "http://..."
+ (fn [] ))
 
+(casper/then
+ (fn [] ))
+
+(casper/run)
