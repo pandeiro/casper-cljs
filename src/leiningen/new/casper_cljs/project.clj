07e68f25eb55f82a39750869036a02f41c41d1bf
Murph <pandeiro@gmail.com>
2012-10-07 23:24:00 -0300

    bumped lein-cljsbuild to 0.2.7

--- src/leiningen/new/casper_cljs/project.clj
+++ src/leiningen/new/casper_cljs/project.clj
@@ -3,7 +3,7 @@
   :url "http://notyet.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ghost "0.1.0-alpha1"]]
-  :plugins [[lein-cljsbuild "0.2.4"]]
+  :plugins [[lein-cljsbuild "0.2.7"]]
   :cljsbuild {:builds [{:source-path "src-cljs"
                         :compiler {:output-to "out/{{sanitized}}.js"
                                    :optimizations :simple, :pretty-print true}}]}
