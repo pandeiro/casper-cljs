d57fae2cb7385174d8272987b1b856ea29a7c3aa
Murphy McMahon <pandeiro@gmail.com>
2014-07-04 11:27:31 -0300

    add explicit cljs dep

--- src/leiningen/new/casper_cljs/project.clj
+++ src/leiningen/new/casper_cljs/project.clj
@@ -2,6 +2,7 @@
   :description "A CasperJS project"
   :url "http://notyet.com"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2261"]
                  [ghost "0.1.0-alpha1"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {:builds [{:source-path "src-cljs"
