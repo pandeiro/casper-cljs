c1250a1ce8df1175a63fe0ad9fcccf6c5ab6decf
Murphy McMahon <pandeiro@gmail.com>
2014-07-04 11:48:41 -0300

    remove license from project

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject casper-cljs/lein-template "0.2.0"
   :description "Basic CasperJS project template"
   :url "https://github.com/pandeiro/casper-cljs"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
