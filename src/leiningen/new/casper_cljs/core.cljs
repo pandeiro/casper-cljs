69701bb94f6b326a61d8bf0c78e29c3af4a976b1
Murphy McMahon <pandeiro@gmail.com>
2014-07-04 11:36:02 -0300

    add actions to template

--- src/leiningen/new/casper_cljs/core.cljs
+++ src/leiningen/new/casper_cljs/core.cljs
@@ -5,11 +5,21 @@
 ;; For complete wrapper API, see:
 ;; https://github.com/pandeiro/ghost/blob/master/src/ghost/core.cljs#L28
 ;;
+(def ua (str "Mozilla/5.0 (X11; Linux x86_64) "
+             "AppleWebKit/536.11 (KHTML, like Gecko) "
+             "Lambdarat/23.0.1062.57"))
+
+(casper/set-casper-options!
+ {:verbose      true
+  :viewportSize {:width 1024 :height 768}
+  :pageSetting  {:loadImages false
+                     :userAgent  ua}})
+
 (casper/start
- "http://..."
- (fn [] ))
+ "http://www.google.com"
+ (fn [] (casper/echo "Loaded Google!")))
 
 (casper/then
- (fn [] ))
+ (fn [] (casper/echo "Still at Google!")))
 
 (casper/run)
